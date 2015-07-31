9708b12c7ba63c9b121d0d693d15c62e9b473767
Alex Bennée <alex.bennee@linaro.org>
2015-07-31 14:03:38 +0100

    barrier test: refator and cleanup

--- arm/barrier-test.c
+++ arm/barrier-test.c
@@ -4,6 +4,8 @@
 #include <asm/barrier.h>
 #include <asm/mmu.h>
 
+#include <prng.h>
+
 #define MAX_CPUS 4
 
 /* How many increments to do */
@@ -14,9 +16,14 @@ static int increment_count = 10000000;
  * doesn't share a page */
 static unsigned int shared_value;
 
+/* PAGE_SIZE * uint32_t means we span several pages */
+static uint32_t memory_array[PAGE_SIZE];
+
 __attribute__((aligned(PAGE_SIZE))) static unsigned int per_cpu_value[MAX_CPUS];
 __attribute__((aligned(PAGE_SIZE))) static cpumask_t smp_test_complete;
 __attribute__((aligned(PAGE_SIZE))) static int global_lock;
+
+struct isaac_ctx prng_context[MAX_CPUS];
 
 void (*inc_fn)(void);
 
@@ -48,6 +55,34 @@ static void increment_shared_with_smp_mb(void)
 	smp_wmb();
 }
 
+/* The idea of this is just to generate some random load/store
+ * activity which may or may not race with an un-barried incremented
+ * of the shared counter
+ */
+static void shuffle_memory(int cpu)
+{
+	int i;
+	uint32_t lspat = isaac_next_uint32(&prng_context[cpu]);
+	uint32_t seq = isaac_next_uint32(&prng_context[cpu]);
+	int count = seq & 0x1f;
+	uint32_t val=0;
+
+	seq >>= 5;
+
+	for (i=0; i<count; i++) {
+		int index = seq & ~PAGE_MASK;
+		if (lspat & 1) {
+			val ^= memory_array[index];
+		} else {
+			memory_array[index] = val;
+		}
+		seq >>= PAGE_SHIFT;
+		seq ^= lspat;
+		lspat >>= 1;
+	}
+
+}
+
 static void do_increment(void)
 {
 	int i;
@@ -58,6 +93,8 @@ static void do_increment(void)
 	for (i=0; i < increment_count; i++) {
 		per_cpu_value[cpu]++;
 		inc_fn();
+
+		shuffle_memory(cpu);
 	}
 
 	printf("CPU%d: Done, %d incs\n", cpu, per_cpu_value[cpu]);
@@ -73,6 +110,8 @@ int main(int argc, char **argv)
 	unsigned int sum = 0;
 
 	inc_fn = &increment_shared;
+
+	isaac_init(&prng_context[0], "myseed", 6);
 	
 	for (i=0; i<argc; i++) {
 		char *arg = argv[i];
@@ -80,17 +119,25 @@ int main(int argc, char **argv)
 		if (strcmp(arg, "lock") == 0) {
 			inc_fn = &increment_shared_with_lock;
 			report_prefix_push("lock");
-		};
-
-		if (strcmp(arg, "smp_mb") == 0) {
+		} else if (strcmp(arg, "smp_mb") == 0) {
 			inc_fn = &increment_shared_with_smp_mb;
 			report_prefix_push("smp_mb");
-		};
+		} else {
+			isaac_reseed(&prng_context[0], arg, strlen(arg));
+		}
+	}
+
+	/* fill our random page */
+	for (i=0; i<PAGE_SIZE; i++) {
+		memory_array[i] = isaac_next_uint32(&prng_context[0]);
 	}
 
 	for_each_present_cpu(cpu) {
+		uint32_t seed = isaac_next_uint32(&prng_context[0]);
 		if (cpu == 0)
 			continue;
+
+		isaac_init(&prng_context[cpu], &seed, sizeof(seed));
 		smp_boot_secondary(cpu, do_increment);
 	}
 
